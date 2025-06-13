2017795ea302e622d407cc636311547297b6758c
Nova <nova@cutey.com>
2025-06-13 01:22:21 +0100

    added copper nugget to gravel washing

--- server_scripts/resource_gen.js
+++ server_scripts/resource_gen.js
@@ -77,4 +77,27 @@ function washingitem(item, aresult, aresultcount, bresult, bresultcount, bresult
 
 ServerEvents.recipes(event => {
     event.custom(Ccrushingitem('minecraft:netherrack', 30, 'kubejs:crushed_netherrack', 1))
+    event.remove({input: 'gravel', output: 'iron_nugget', type: 'create:splashing'})
+    event.custom({
+  "type": "create:splashing",
+  "ingredients": [
+    {
+      "item": "minecraft:gravel"
+    }
+  ],
+  "results": [
+    {
+      "chance": 0.25,
+      "id": "minecraft:flint"
+    },
+    {
+      "chance": 0.125,
+      "id": "minecraft:iron_nugget"
+    },
+    {
+        chance: 0.125,
+        id: 'minecraft:copper_nugget'
+    }
+  ]
+})
 })
